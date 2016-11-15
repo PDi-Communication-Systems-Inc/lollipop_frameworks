d8ac804f6daa25e185d705583c739d911d738adb
mrobbeloth-pdi <mrobbeloth@pdiarm.com>
2016-11-15 16:08:29 -0500

    Have the soft keyboard always show up

--- base/core/java/android/inputmethodservice/InputMethodService.java
+++ base/core/java/android/inputmethodservice/InputMethodService.java
@@ -1132,9 +1132,7 @@ public class InputMethodService extends AbstractInputMethodService {
      * re-evaluated and applied.
      */
     public boolean onEvaluateInputViewShown() {
-        Configuration config = getResources().getConfiguration();
-        return config.keyboard == Configuration.KEYBOARD_NOKEYS
-                || config.hardKeyboardHidden == Configuration.HARDKEYBOARDHIDDEN_YES;
+	return true;
     }
     
     /**
